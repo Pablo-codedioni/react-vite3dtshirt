33e40902d882a167bf6b99a681715e49bf7eecda
Carlos969988 <136388389+Carlos969988@users.noreply.github.com>
2023-10-04 13:38:22 -0600

    Rama

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s7tfan3o1no"
+    "revision": "0.41g5otec37"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
