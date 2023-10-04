0a0b56aa471fdf1f89be9811f6ba7eb967e66e0b
Pablo-codedioni <uriel-nike@outlook.com>
2023-10-03 20:09:51 -0500

    dev-dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5t52kluqdig"
+    "revision": "0.n7r03c022jo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
